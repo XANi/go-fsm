896b25bdcb6f72180c257abcba2ab3789b5120d7
Mariusz Gronczewski <xani666@gmail.com>
2017-06-18 00:05:56 +0200

    working state machine

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 type FSM struct {
 	states map[int]*State
+	state int
 }
 
 // State() returns existing state struct.
@@ -16,15 +17,32 @@ func (f *FSM) State(s int) *State {
 		f.states[s] =&State{
 			fsm: f,
 			id: s,
-			next: make(map[int]int),
+			next: make(map[int]func() bool),
 		}
 		return f.states[s]
 	}
 }
 
+func (f *FSM) Go(s int) bool {
+	if verifyFunc, ok := f.states[f.state].next[s] ; ok {
+		if verifyFunc == nil {
+			f.state = s
+			return true
+		} else {
+			isOk := verifyFunc()
+			if isOk {
+				f.state = s
+			}
+			return isOk
+		}
+	} else {
+		return false
+	}
+}
+
 type State struct {
 	fsm *FSM
-	next map[int]int
+	next map[int]func() bool
 	id int
 }
 
@@ -43,12 +61,18 @@ func (st *State) Next() []int {
 type Transitions struct{
 	From int
 	To []int
-	Condition *func() bool
+	Condition func() bool
 }
 
 func New(startingState int, stateTable []Transitions) (*FSM, error) {
 	f :=  FSM{
 		states: make(map[int]*State),
+		state: startingState,
+	}
+	for _, transition := range stateTable {
+		for _, trTo := range transition.To {
+			f.State(transition.From).next[trTo] = transition.Condition
+		}
 	}
 	return &f, nil
 }
--- main_test.go
+++ main_test.go
@@ -18,20 +18,68 @@ const (
 
 
 func TestState(t *testing.T) {
-	states := []Transitions{
-		{From: StStart, To: []int{StPr1Step1, StPr2Step1}},
-		{From: StPr1Step1, To: []int{StPr1Step1, StPr2Step2}},
-		{From: StPr1Step2, To: []int{StPr1Step3}},
-		{From: StPr1Step3, To: []int{StEnd}},
-		{From: StStart, To: []int{StPr1Step1, StPr2Step1}},
-		{From: StPr2Step1, To: []int{StPr2Step2}},
-		{From: StPr2Step2, To: []int{StPr1Step3}},
-		{From: StPr2Step3, To: []int{StEnd}},
-	}
-	sm, _ := New(StStart,states)
+	sm := prepareTestObject()
 	Convey("GetNextState",t,func() {
 		So(	sm.State(StStart).Next(),ShouldContain,StPr1Step1)
 		So(	sm.State(StStart).Next(),ShouldContain,StPr2Step1)
 	})
+	Convey("Transition to valid state",t,func() {
+		So (sm.Go(StPr1Step1),ShouldBeTrue)
+		So (sm.Go(StPr1Step2),ShouldBeTrue)
+	})
+	Convey("Transition to invalid state",t,func() {
+		So (sm.Go(StStart),ShouldBeFalse)
+	})
+	sm = prepareTestObject()
+	Convey("Validation Function",t,func() {
+		So(sm.Go(StPr2Step1),ShouldBeTrue)
+		So(sm.Go(StPr2Step2),ShouldBeFalse)
 
+
+	})
+
+
+
+
+
+}
+
+
+func prepareTestObject() *FSM {
+	states := []Transitions{
+		{From: StStart, To: []int{StPr1Step1}, },
+		{From: StPr1Step1, To: []int{StPr1Step1, StPr1Step2, StPr1Step2}},
+		{From: StPr1Step2, To: []int{StPr1Step3}},
+		{From: StPr1Step3, To: []int{StEnd}},
+		{From: StStart, To: []int{StPr2Step1},Condition: func() bool {return true}},
+		{From: StPr2Step1, To: []int{StPr2Step2,StPr2Step1}, Condition: func() bool {return false}},
+		{From: StPr2Step2, To: []int{StPr1Step3}},
+		{From: StPr2Step3, To: []int{StEnd}},
+	}
+	sm, _ := New(StStart,states)
+	return sm
+}
+
+
+func BenchmarkNextState(b *testing.B) {
+	sm := prepareTestObject()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		sm.State(StStart).Next()
+	}
+}
+func BenchmarkStateTransition(b *testing.B) {
+	sm := prepareTestObject()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		sm.Go(StPr1Step1)
+	}
+}
+
+func BenchmarkStateTransitionWithCondition(b *testing.B) {
+	sm := prepareTestObject()
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		sm.Go(StPr2Step1)
+	}
 }
